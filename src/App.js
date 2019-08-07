1921871101c7b9b63afbbc7c95ac60c7007fdf35
bassiebangura <devbassiebangura@gmail.com>
2019-08-07 19:50:56 -0400

    Add radio button. Checkbox is functional.

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ class App extends Component {
     this.state = {
       firstName: "First Name",
       lastName: "Last Name",
-      isFriendly: false
+      isFriendly: false,
+      gender: ""
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    const { name, value, type, checked  } = event.target;
-    type === checked ? this.setState({[name] : checked}) : this.setState({
-      [name]: value
-    });
+    const { name, value, type, checked } = event.target;
+    type === "checkbox"
+      ? this.setState({ [name]: checked })
+      : this.setState({
+          [name]: value
+        });
   }
   render() {
     return (
@@ -24,8 +27,7 @@ class App extends Component {
         <input
           name="firstName"
           type="text"
-       
-          value={this.state.firstName}//*this creates controlled input; resulting in single source of truth 
+          value={this.state.firstName} //*this creates controlled input; resulting in single source of truth
           onChange={this.handleChange}
         />
 
@@ -33,47 +35,45 @@ class App extends Component {
         <input
           name="lastName"
           type="text"
-       
-    
-          value={this.state.lastName}//this creates controlled input; resulting in single source of truth 
-
+          value={this.state.lastName} //this creates controlled input; resulting in single source of truth
           onChange={this.handleChange}
         />
         <br />
-         
-        <textarea 
-                    value={"Some default value"}
-                    onChange={this.handleChange}
-                />
-                
-                <br />
-                
-                <label>
-                    <input 
-                        type="checkbox" 
-                        name="isFriendly"
-                        checked={this.state.isFriendly}
-                        onChange={this.handleChange}
-                    /> Is friendly?
-                </label>
-                <label>
-                    <input 
-                        type="radio" 
-                        name="gender"
-                        checked={this.state.isFriendly}
-                        onChange={this.handleChange}
-                    /> Male
-                </label>
-                <label>
-                    <input 
-                        type="radio" 
-                        name="gender"
-                        checked={this.state.isFriendly}
-                        onChange={this.handleChange}
-                    /> Female
-                </label>
+
+        <textarea value={"Some default value"} onChange={this.handleChange} />
+
+        <br />
+
+        <label>
+          <input
+            type="checkbox"
+            name="isFriendly"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          />
+          Is friendly?
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={this.state.gender === "male"}
+            onChange={this.handleChange}
+          />{" "}
+          Male
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={this.state.gender === "female"}
+            onChange={this.handleChange}
+          />{" "}
+          Female
+        </label>
         <div>{`${this.state.firstName} ${this.state.lastName}`}</div>
-       
       </form>
     );
   }
